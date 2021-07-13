d731d873dcdd6bfb0a8634715a4d649023940aee
rajan singh <rajanranjan06254@gmail.com>
2021-07-13 12:27:40 +0530

    first commit

---
+++ connect/connection.php
@@ -0,0 +1,16 @@
+<?php 
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "Rajan@#123";
+$dbName = "mailing";
+
+$connect = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
+
+/*
+// Check connection
+if ($mysqli -> connect_error) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}*/
+?>
